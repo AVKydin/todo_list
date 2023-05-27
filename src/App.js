1f15f1091018ef0ceede5dafb2349a99b6dbb240
Q1 <tlalok@ukr.net>
2023-05-27 11:41:56 +0300

    початок2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-import './App.css';
-import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React, { useState } from "react";
+import NewCost from "./components/NewCost/NewCost";
+import Costs from "./components/Costs/Costs";
 
-import {Header} from "./components/header";
-import {Friday, Monday, Saturday, Sunday, Thursday, Tuesday, Wednesday} from "./components/daysOfTheWeek";
+const INITIAL_COSTS = [
+  {
+    id: "c1",
+    date: new Date(2020, 2, 12),
+    description: "Холодильник",
+    amount: 999.99,
+  },
+  {
+    id: "c2",
+    date: new Date(2021, 11, 25),
+    description: "MacBook",
+    amount: 1254.72,
+  },
+  {
+    id: "c3",
+    date: new Date(2021, 3, 1),
+    description: "Джинсы",
+    amount: 49.99,
+  },
+];
 
+const App = () => {
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Начнём изучение React!"),
+  //   React.createElement(Costs, { costs: costs })
+  // );
 
-function App() {
+  const addCostHandler = (cost) => {
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
+  };
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path={'/'} element={<Header/>}>
-              <Route path={'monday'} element={<Monday/>}></Route>
-              <Route path={'tuesday'} element={<Tuesday/>}></Route>
-              <Route path={'wednesday'} element={<Wednesday/>}></Route>
-              <Route path={'thursday'} element={<Thursday/>}></Route>
-              <Route path={'friday'} element={<Friday/>}></Route>
-              <Route path={'saturday'} element={<Saturday/>}></Route>
-              <Route path={'sunday'} element={<Sunday/>}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
+    <div>
+      <NewCost onAddCost={addCostHandler} />
+      <Costs costs={costs} />
     </div>
   );
-}
+};
 
 export default App;
